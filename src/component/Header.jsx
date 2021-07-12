597a4e3a7070683d43a98ca6b77e9ba04404f974
HARSHIT SRIVASTAVA <54814874+harshit051@users.noreply.github.com>
2021-07-12 18:59:20 +0530

    Add files via upload

---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
+
+export default function Header() {
+    return(<header>
+        <h1>
+        <EmojiObjectsIcon/>
+        Keep Note
+        </h1>
+      </header>);
+}
